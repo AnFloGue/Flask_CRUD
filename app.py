7951c55a41da4f92af78525eede54c11bd0d0798
ANflo <75026239+AnFloGue@users.noreply.github.com>
2024-07-16 13:21:22 +0200

    03 Commit: create the new templates and the CRUD logic in app.py

--- app.py
+++ app.py
@@ -3,35 +3,28 @@ import json
 
 app = Flask(__name__)
 
-# Step 2: Read the JSON file
 with open('blog_posts.json', 'r') as file:
     blog_posts = json.load(file)
 
 
 @app.route('/')
 def index():
-    # Step 4: Pass the data to the template
     return render_template('index.html', posts=blog_posts)
 
 
 def get_next_id():
-    if not blog_posts:
-        return 1
-    max_id = max(post['id'] for post in blog_posts)
-    return max_id + 1
+    return max((post['id'] for post in blog_posts), default=0) + 1
 
 
 @app.route('/add', methods=['GET', 'POST'])
 def add():
     if request.method == 'POST':
-        title = request.form.get('title')
-        author = request.form.get('author')
-        content = request.form.get('content')
         new_post = {
-            'id': get_next_id(),  # Generate the next ID
-            'title': title,
-            'author': author,
-            'content': content
+            'id': get_next_id(),
+            'title': request.form.get('title'),
+            'author': request.form.get('author'),
+            'content': request.form.get('content'),
+            'likes': 0
         }
         blog_posts.append(new_post)
         with open('blog_posts.json', 'w') as file:
@@ -43,10 +36,26 @@ def add():
 @app.route('/delete/<int:post_id>')
 def delete(post_id):
     global blog_posts
-    blog_posts = [post for post in blog_posts if post['id'] != post_id]
+    updated_blog_posts = []
+    for post in blog_posts:
+        if post['id'] != post_id:
+            updated_blog_posts.append(post)
+    blog_posts = updated_blog_posts
     with open('blog_posts.json', 'w') as file:
         json.dump(blog_posts, file)
     return redirect(url_for('index'))
+
+# alternative to delete
+# @app.route('/delete/<int:post_id>')
+# def delete(post_id):
+#     global blog_posts
+#     for i, post in enumerate(blog_posts):
+#         if post['id'] == post_id:
+#             blog_posts.pop(i)
+#             break
+#     with open('blog_posts.json', 'w') as file:
+#         json.dump(blog_posts, file)
+#     return redirect(url_for('index'))
 
 
 def fetch_post_by_id(post_id):
@@ -59,18 +68,44 @@ def fetch_post_by_id(post_id):
 @app.route('/update/<int:post_id>', methods=['GET', 'POST'])
 def update(post_id):
     post = fetch_post_by_id(post_id)
-    if post is None:
-        return "Post not found", 404
-
+    
+    if not post:
+        return "The blog post was not found", 404
+    
     if request.method == 'POST':
-        post['title'] = request.form.get('title')
-        post['author'] = request.form.get('author')
-        post['content'] = request.form.get('content')
+        post.update({
+            'title': request.form.get('title'),
+            'author': request.form.get('author'),
+            'content': request.form.get('content')
+        })
+        
         with open('blog_posts.json', 'w') as file:
             json.dump(blog_posts, file)
         return redirect(url_for('index'))
-
     return render_template('update.html', post=post)
+
+
+@app.route('/like/<int:post_id>', methods=['POST'])
+def like(post_id):
+    post = fetch_post_by_id(post_id)
+    if not post:
+        return "The blog post was not found", 404
+    post['likes'] += 1
+    with open('blog_posts.json', 'w') as file:
+        json.dump(blog_posts, file)
+    return redirect(url_for('index'))
+
+
+@app.route('/dislike/<int:post_id>', methods=['POST'])
+def dislike(post_id):
+    post = fetch_post_by_id(post_id)
+    if not post:
+        return "The blog post was not found", 404
+    if post['likes'] > 0:
+        post['likes'] -= 1
+    with open('blog_posts.json', 'w') as file:
+        json.dump(blog_posts, file)
+    return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
